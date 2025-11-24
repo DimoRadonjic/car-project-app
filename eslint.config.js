0ff3b6f9fd23e8338cecdac18917b71f2bfa1220
Dimitrije Radonjić <dimo.radonjic2112@gmail.com>
2025-11-24 15:34:14 +0100

    new lint rule added for vue : file order tags

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,7 @@ export default defineConfigWithVueTs(
    * pluginVue.configs["flat/recommended"]
    *   -> Above, plus rules to enforce subjective community defaults to ensure consistency.
    */
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs['flat/recommended'],
 
   {
     files: ['**/*.ts', '**/*.vue'],
@@ -64,6 +64,8 @@ export default defineConfigWithVueTs(
     // add your custom rules here
     rules: {
       'prefer-promise-reject-errors': 'off',
+
+      'vue/block-order': ['warn', { order: ['script', 'template', 'style'] }],
 
       // allow debugger during development only
       'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
